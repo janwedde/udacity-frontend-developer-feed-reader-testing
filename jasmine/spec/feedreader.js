acd51f07f53b9a36a0e6a112529cdf5d34a52d61
Jan Weddehage <49489658+janwedde@users.noreply.github.com>
2019-05-01 12:04:27 +0200

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* Write a test that loops through each feed
+        /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -42,7 +42,7 @@ $(function() {
            });
          });
 
-        /* Write a test that loops through each feed
+        /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -56,7 +56,7 @@ $(function() {
          });
     });
 
-    /* Write a new test suite named "The menu" */
+    /* TODO: Write a new test suite named "The menu" */
     describe("The menu", function() {
       // Creates variable for body
       const body = document.querySelector("body");
@@ -69,7 +69,7 @@ $(function() {
            // Checks if class for hidden menu exists in body
            expect(body.classList).toContain("menu-hidden");
          });
-         /* Write a test that ensures the menu changes
+         /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -85,9 +85,9 @@ $(function() {
            expect(body.classList).toContain("menu-hidden");
          });
     });
-    /* Write a new test suite named "Initial Entries" */
+    /* TODO: Write a new test suite named "Initial Entries" */
     describe("Initial Entries", function() {
-        /* Write a test that ensures when the loadFeed
+        /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -103,18 +103,18 @@ $(function() {
          it("have at least a count of one", function (done) {
            // Create variable for feed entrys
            const entry = document.querySelector(".feed .entry");
-           // Number of entries should be > 0
+           // Number of feed entries should be > 0
            expect(entry.length).not.toBe(0);
            done();
          });
     });
-    /* Write a new test suite named "New Feed Selection" */
+    /* TODO: Write a new test suite named "New Feed Selection" */
     describe("New Feed Selection", function() {
       // Create variable for first load of feed
       let firstRun;
       // Create variable for second load of feed
       let secondRun;
-        /* Write a test that ensures when a new feed is loaded
+        /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
@@ -123,11 +123,11 @@ $(function() {
            loadFeed(0, function() {
              // Store html feed of first load in variable
              firstRun = $(".feed").html();
-           });
-           loadFeed(1, function() {
-             // Store html feed of second load in variable
-             secondRun = $(".feed").html();
-             done();
+               loadFeed(1, function() {
+                // Store html feed of second load in variable
+                secondRun = $(".feed").html();
+                done();
+              });
            });
          });
          // Test if html feed changes after second run
